1794ac6be9c408498cc72b5751ac37e0b9040e3c
thepoppinshowofficial-byte <thepoppinshowofficial@gmail.com>
2026-08-31 14:55:18 +0530

    Update site.js

--- site.js
+++ site.js
@@ -1,5 +1,5 @@
 
-const AFFILIATE_LINK = "YOUR_GREYHOUND_AFFILIATE_LINK";
+const AFFILIATE_LINK = "https://admin.rewardoo.com/track/e5cd75GWpnwD1N3Rheo_axUrHQ1vyWkN61szWbOjqJLQsLKvv6pUHDLSbORGdER4L4Bw4ZsQ4SpgxeH0_c?source=inner&url=https%3A%2F%2Fwww.greyhound.com";
 
 document.addEventListener("DOMContentLoaded",()=>{
   document.querySelectorAll("[data-affiliate]").forEach(a=>{
